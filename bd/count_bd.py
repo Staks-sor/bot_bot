90a6fbbf3b0f83c7a2327206a3be4064399126e1
staks_sor <stas.sor@gmail.com>
2022-07-08 14:28:21 +0300

    добавил апдейт базы. селект базы индекса

--- bd/count_bd.py
+++ bd/count_bd.py
@@ -5,6 +5,7 @@ from bd.config import host, user, password, db_name, port
 
 
 def get_connect_heroku_bd(zodiac, id):
+    global connection
     try:
         connection = psycopg2.connect(
             host=host,
@@ -17,7 +18,7 @@ def get_connect_heroku_bd(zodiac, id):
 
         with connection.cursor() as cursor:
             cursor.execute(
-                f"""SELECT {zodiac} FROM goroskop WHERE _id = {id};"""
+                f"""SELECT {zodiac} FROM gor WHERE _id = {id};"""
             )
             text_cursor_bd = cursor.fetchone()
             print(text_cursor_bd[0])
@@ -32,65 +33,64 @@ def get_connect_heroku_bd(zodiac, id):
             print("[INFO] PostgreSQL connection closed")
 
 
-# def get_id():
-#     try:
-#         connection = psycopg2.connect(
-#             host=host,
-#             user=user,
-#             password=password,
-#             database=db_name,
-#             port=port
-#         )
-#         connection.autocommit = True
-#
-#         with connection.cursor() as cursor:
-#             cursor.execute(
-#                 """INSERT INTO bot_id (id) VALUES
-#                 (5);"""
-#             )
-#
-#             print("[INFO] Data was succefully inserted")
-#
-#     except Exception as _ex:
-#         print("[INFO] Error while working with PostgreSQL", _ex)
-#     finally:
-#         if connection:
-#             # cursor.close()
-#             connection.close()
-#             print("[INFO] PostgreSQL connection closed")
-#
-#
-#
-#
-# def get_id_index():
-#     try:
-#         connection = psycopg2.connect(
-#             host=host,
-#             user=user,
-#             password=password,
-#             database=db_name,
-#             port=port
-#         )
-#         connection.autocommit = True
-#
-#         with connection.cursor() as cursor:
-#             cursor.execute(
-#                 f"""SELECT * FROM bot_id WHERE id = 1;"""
-#             )
-#             text_cursor_bd = cursor.fetchone()
-#             print(text_cursor_bd[0])
-#             return text_cursor_bd[0]
-#
-#     except Exception as _ex:
-#         print("[INFO] Error while working with PostgreSQL", _ex)
-#     finally:
-#         if connection:
-#             # cursor.close()
-#             connection.close()
-#             print("[INFO] PostgreSQL connection closed")
+def get_id(id=8):
+    try:
+        connection = psycopg2.connect(
+            host=host,
+            user=user,
+            password=password,
+            database=db_name,
+            port=port
+        )
+        connection.autocommit = True
+
+        with connection.cursor() as cursor:
+            cursor.execute(
+                f"""UPDATE bot_id SET id = {id};"""
+            )
+
+            print("[INFO] Data was succefully inserted")
+
+    except Exception as _ex:
+        print("[INFO] Error while working with PostgreSQL", _ex)
+    finally:
+        if connection:
+            # cursor.close()
+            connection.close()
+            print("[INFO] PostgreSQL connection closed")
+
+
+
+
+def get_id_index():
+    try:
+        connection = psycopg2.connect(
+            host=host,
+            user=user,
+            password=password,
+            database=db_name,
+            port=port
+        )
+        connection.autocommit = True
+
+        with connection.cursor() as cursor:
+            cursor.execute(
+                f"""SELECT * FROM bot_id;"""
+            )
+            text_cursor_bd = cursor.fetchone()
+            print(text_cursor_bd[0])
+            return text_cursor_bd[0]
+
+    except Exception as _ex:
+        print("[INFO] Error while working with PostgreSQL", _ex)
+    finally:
+        if connection:
+            # cursor.close()
+            connection.close()
+            print("[INFO] PostgreSQL connection closed")
 
 
 if __name__ == "__main__":
     get_connect_heroku_bd(zodiac="Овен", id=1)
-    # get_id()
-    # get_id_index()
+    get_id()
+    get_id_index()
